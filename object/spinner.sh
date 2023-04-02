638ebf9f9270141738b5bb0ac47c6406125a895c
* <bodroadikorooo>
2023-04-02 14:21:57 +0700

    v1

---
+++ object/spinner.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+function _spinner() {
+
+    local on_success="DONE"
+    local on_fail="FAIL"
+    local white="\e[1;37m"
+    local green="\e[1;32m"
+    local red="\e[1;31m"
+    local nc="\e[0m"
+
+    case $1 in
+        start)
+
+            let column=$(tput cols)-${#2}-8
+            echo -ne ${2}
+            printf "%${column}s"
+
+            i=1
+            sp='\|/-'
+            delay=${SPINNER_DELAY:-0.15}
+
+            while :
+            do
+                printf "\b${sp:i++%${#sp}:1}"
+                sleep $delay
+            done
+            ;;
+        stop)
+            if [[ -z ${3} ]]; then
+                echo "spinner is not running.."
+                exit 1
+            fi
+
+            kill $3 > /dev/null 2>&1
+
+            echo -en "\b["
+            if [[ $2 -eq 0 ]]; then
+                echo -en "${green}${on_success}${nc}"
+            else
+                echo -en "${red}${on_fail}${nc}"
+            fi
+            echo -e "]"
+            ;;
+        *)
+            echo "invalid argument, try {start/stop}"
+            exit 1
+            ;;
+    esac
+}
+
+function start_spinner {
+    _spinner "start" "${1}" &
+    _sp_pid=$!
+    disown
+}
+
+function stop_spinner {
+    _spinner "stop" $1 $_sp_pid
+    unset _sp_pid
+}
+
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+pkg install ncurses-utils -y > /dev/null 2>&1
+
+path_object="$(pwd)/object"
+
+source "$path_object/spinner.sh"
+
+packages=('git' 'lsd' 'tree' 'python' 'nodejs-lts')
+
+for package in "${packages[@]}"; do
+	start_spinner $package
+	apt install $package -y > /dev/null 2>&1
+	stop_spinner $?
+done
+
+cp -rf "$path_object" $HOME
+mv $HOME/object/bashrc $HOME/object/.bashrc
+mv $HOME/object $HOME/.object 
+mv -f $HOME/.object/.bashrc $HOME/
+
+rm -f $HOME/../usr/etc/motd
+
+mkdir $HOME/.termux
+cd && cd .termux && start_spinner "Downloading Font" && curl -fLo font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip > /dev/null 2>&1 && stop_spinner $? && mkdir fonts && mv -f font.zip fonts && cd fonts && unzip font.zip > /dev/null 2>&1 && mv -f "Hack Regular Nerd Font Complete.ttf" .. && cd .. && mv "Hack Regular Nerd Font Complete.ttf" font.ttf && rm -rf fonts && cd
+
+mv -f $HOME/.object/colors.properties $HOME/.termux/
+node_modules_my_dev=('live-server' 'sass' 'typescript')
+
+for node in "${node_modules_my_dev[@]}"; do
+	start_spinner $node
+	npm install -g $node -y > /dev/null 2>&1
+	stop_spinner $?
+done
+
+echo "Exit and open Again, NOW!!!"
